d13e102b544eee33a28005832e2520e53cc255e5
mehulmayavanshi278 <mehulmayavanshi0867@gmail.com>
2023-04-22 19:30:42 +0530

    new commit

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static(path.join(__dirname,"/images")));
 console.log(path.join(__dirname,"/images"));
 app.use(cors({
-  origin:"http://localhost:3000"
+  origin:"https://admitcardfrontend.netlify.app"
 }));
 app.use(bodyParser.json())
 app.use( bodyParser.urlencoded({extended: true }));
